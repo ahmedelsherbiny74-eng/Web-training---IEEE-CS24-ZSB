2721a7e9d30f45b6e2c743ab99802ada85b11bd6
ahmedelsherbiny74-eng <ahmedelsherbiny7474@gmail.com>
2024-05-07 22:18:11 +0300

    Task-13

---
+++ JS/Task-13/Assignments from 023-to-026/assignment-1.js
@@ -0,0 +1,13 @@
+console.log(100_000);
+console.log(100000);
+console.log(5e4 + 5e4);
+console.log(10_00_00);
+console.log(10 ** 5);
+console.log(200 * 500);
+console.log(Math.pow(10, 5));
+console.log(Math.round(99999.99));
+console.log(Math.ceil(99999.99));
+console.log(Math.floor(100000.01));
+console.log(Math.trunc(100000.01));
+console.log(Math.min(100001, 100000));
+console.log(Math.max(100000, 99999));
